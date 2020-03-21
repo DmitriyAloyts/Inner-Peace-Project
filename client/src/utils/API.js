8ad3e8bbd7d8f5bcfc78542faa24db1c2865ae3a
Dmitriy Aloyts <dmitriy@iinet.net.au>
2020-03-21 21:31:45 +1100

    remove 3001port from utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,22 +3,23 @@ import axios from "axios";
 export default {
   // Gets all posts
   getPosts: function() {
-    return axios.get("http://localhost:3001/api/posts");
+    return axios.get("api/posts");
   },
   // Gets the post with the given id
   getPost: function(id) {
-    return axios.get("http://localhost:3001/api/posts/" + id);
+    // return axios.get("http://localhost:3001/api/posts/" + id);
+    return axios.get("/api/posts/" + id);
   },
   // Deletes the post with the given id
   deletePost: function(id) {
-    return axios.delete("http://localhost:3001/api/posts/" + id);
+    return axios.delete("/api/posts/" + id);
   },
   // Saves a post to the database
   savePost: function(postData) {
-    return axios.post("http://localhost:3001/api/posts", postData);
+    return axios.post("/api/posts", postData);
   },
   
   saveComment: function(commentData) {
-    return axios.post("http://localhost:3001/api/comments", commentData);
+    return axios.post("/api/comments", commentData);
   }
 };
